8b63fa1421f721b3f52b7c7735dcc87b729896fd
Murilo Nascimento <murilojesus659@gmail.com>
2024-05-23 15:53:13 -0300

    creating db connection and using migrations

--- resources/views/products.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-@section('title', 'Produtos')
-@section('content')
-  <h1>Página de produtos</h1>
-  @if($search !== null)
-    <p>O usuário está buscando por: {{ $search }}</p>
-  @endif
-@endsection
