d9531c373a8b0140823938a97f9cf3f623cc4053
Murilo Nascimento <murilojesus659@gmail.com>
2024-06-05 16:27:21 -0300

    creating dashboard page

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
+Route::get('/dashboard', [EventController::class, 'dashboard'])
+->middleware('auth');
 Route::get('/events/create', [EventController::class, 'create'])
 ->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
